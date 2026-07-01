a0285ce75a7a56db040642cbbda284851313d5fa
tpark747949 <tpark747949@gmail.com>
2026-06-30 21:33:16 -0400

    Installed EVA-X reqs.

--- eval/EVA-X/sample_extract.py
+++ eval/EVA-X/sample_extract.py
@@ -1,3 +1,12 @@
+import sys
+from pathlib import Path
+
+# 1. Point Python to the cloned EVA-X subfolder
+eva_x_dir = Path(__file__).parent / "EVA-X"
+sys.path.append(str(eva_x_dir))
+
 from eva_x import eva_x_tiny_patch16, eva_x_small_patch16, eva_x_base_patch16
 
-model = eva_x_small_patch16(pretrained=/path/to/pre-trained)
+print("all good.")
+
+# model = eva_x_small_patch16(pretrained=/path/to/pre-trained)
